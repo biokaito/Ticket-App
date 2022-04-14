b64bcb7a9a29cc924fb71a300645f8f2a3fef7f5
Trieu Nguyen <bastrieu281@gmail.com>
2022-04-15 01:17:38 +0700

    Authentication Strategies and Options

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) =>{
-    res.send("Hehe")
+router.get('/api/users/currentuser',currentUser,requireAuth, (req, res) => {
+    res.send({currentUser: req.currentUser || null});
 });
 
-export {router as currentUserRouter};
+export { router as currentUserRouter };
